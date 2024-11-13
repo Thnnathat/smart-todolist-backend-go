aca8578c8e49bd82d65dbc8923bf5c81baaa87c3
Thnnathat Chaiphutha <thnnathatchaiphutha@gmail.com>
2024-11-13 14:27:09 +0700

    hash password

--- cores/user/usecases/userUsecaseImpl.go
+++ cores/user/usecases/userUsecaseImpl.go
@@ -3,6 +3,7 @@ package usecases
 import (
 	"github.com/Thnnathat/smart-todolist-backend-go/cores/user/entities"
 	"github.com/Thnnathat/smart-todolist-backend-go/cores/user/repositories"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type userUsecaseImpl struct {
@@ -18,6 +19,14 @@ func (u *userUsecaseImpl) CreateUser(user *entities.User) error {
 		Username: user.Username,
 		Password: user.Password,
 	}
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(createUser.Password), bcrypt.DefaultCost)
+
+	if err != nil {
+		return err
+	}
+
+	createUser.Password = string(hashedPassword)
 
 	if err := u.userRepository.SaveUser(createUser); err != nil {
 		return err
